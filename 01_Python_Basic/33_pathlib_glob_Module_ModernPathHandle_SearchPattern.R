582a62efcff433a53385109eb9874d44f6c486af
Longistamina <dophamthelong03@gmail.com>
2025-07-22 23:27:05 +0700

    prepared file for pathlib

---
+++ 01_Python_Basic/33_pathlib_glob_Module_ModernPathHandle_SearchPattern.R
@@ -0,0 +1,9 @@
+'''
+Pathlib is a module in Python that provides an object-oriented approach to handle filesystem paths. 
+The `glob` method in the `pathlib` module allows you to search for files and directories matching a specified pattern.
+
+Unlike the traditional `os` module, `pathlib` provides a more intuitive and readable way to work with paths.
+
+Flow of contents:
+
+'''
