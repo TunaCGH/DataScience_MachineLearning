7a3ab62ca78abb2b460306a7e34532111c401459
Longistamina <dophamthelong03@gmail.com>
2025-08-26 21:30:24 +0700

    prepared for BoleanFiltering

---
+++ 05_Pandas_DataR_dataframe/01_Pandas_python_style/1_Series_15_BooleanFiltering_between_isin.py
@@ -0,0 +1,20 @@
+'''
+Boolean Filtering or Boolean Indexing is a powerful technique in Pandas 
+that allows you to filter data based on specific conditions. 
+
+###############################
+
+Flow of contents:
+
+1. Single Condition Examples:
+   + Logic Operators: ==, !=, >, <, >=, <=, .between() 
+   + .isin()
+   + String Boolean: .str.contains(), .str.startswith(), .str.endswith(), ....
+   + DateTime Boolean: .dt.is_month_start, .dt.is_month_end, ...
+
+2. Negation of Condition: ~ (tilde) operator
+
+3. Combine Multiple Conditions:
+   + & (and),
+   + | (or)
+'''
