b34c5b78a209855d20e323ea5274d8909ff37478
Longistamina <dophamthelong03@gmail.com>
2025-09-03 10:29:52 +0700

    DF 16 completed df.eval()

--- 05_Pandas_DataR_dataframe/01_Pandas_python_style/2_DataFrame_16_modify_derive_cols_assign_eval.py
+++ 05_Pandas_DataR_dataframe/01_Pandas_python_style/2_DataFrame_16_modify_derive_cols_assign_eval.py
@@ -199,3 +199,88 @@ print(df_demo.head(3))
 # 0    Adam_Donachie  bal      74     180     187.96   81.64656  0.002311
 # 1        Paul_Bako  bal      74     215     187.96   97.52228  0.002760
 # 2  Ramon_Hernandez  bal      72     210     182.88   95.25432  0.002848
+
+
+#---------------------------------------------------------------------------------------------------------------#
+#------------------------------------------- 3. Using df.eval() ------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------------#
+
+'''
+Support operators: +, -, *, /, //, %, ** and parentheses () for grouping.
+
+If the column name has special characters => put in backticks ``: `variable name` (like SQL style)
+
+Can use @ to reference variables outside the DataFrame like: @variable_name.
+'''
+
+inch_to_m = 0.0254
+lbs_to_kg = 0.453592
+
+#####################################
+##     Modify existing columns     ##
+#####################################
+'''df.eval("existing_col = ...", inplace = True/False)'''
+
+#-----------------
+## Modify a single column: Weight from lbs to kg
+#-----------------
+
+df_demo = df_baseball.copy().eval("Weight = Weight * @lbs_to_kg", inplace = False)
+print(df_demo.head(3))
+#               Name Team  Height    Weight
+# 0    Adam_Donachie  BAL      74  81.64656
+# 1        Paul_Bako  BAL      74  97.52228
+# 2  Ramon_Hernandez  BAL      72  95.25432
+
+#-----------------
+## Modify multiple columns
+#-----------------
+
+df_demo = df_baseball.copy().eval(
+    """
+    Height = Height * @inch_to_m
+    Weight = Weight * @lbs_to_kg
+    Team = Team.str.lower().astype('category')
+    """
+)
+
+print(df_demo.head(3))
+#               Name Team  Height    Weight
+# 0    Adam_Donachie  bal  1.8796  81.64656
+# 1        Paul_Bako  bal  1.8796  97.52228
+# 2  Ramon_Hernandez  bal  1.8288  95.25432
+
+################################
+##     Derive new columns     ##
+################################
+'''df.eval("new_col = ...", inplace = True/False)'''
+
+#-----------------
+## Derive single column (apply ``)
+#-----------------
+
+df_demo = df_baseball.copy().eval("BMI = `Weight` / (`Height` ** 2)", inplace = False)
+print(df_demo.head(3))
+#               Name Team  Height  Weight       BMI
+# 0    Adam_Donachie  BAL      74     180  0.032871
+# 1        Paul_Bako  BAL      74     215  0.039262
+# 2  Ramon_Hernandez  BAL      72     210  0.040509
+
+#-----------------
+## Derive multiple columns
+#-----------------
+
+df_demo = df_baseball.copy().eval(
+    """
+    height_m = Height * @inch_to_m
+    weight_kg = Weight * @lbs_to_kg
+    bmi = weight_kg / (height_m ** 2)
+    """,
+    inplace = False
+)
+
+print(df_demo.head(3))
+#               Name Team  Height  Weight  height_m  weight_kg        bmi
+# 0    Adam_Donachie  BAL      74     180    1.8796   81.64656  23.110376
+# 1        Paul_Bako  BAL      74     215    1.8796   97.52228  27.604061
+# 2  Ramon_Hernandez  BAL      72     210    1.8288   95.25432  28.480805
