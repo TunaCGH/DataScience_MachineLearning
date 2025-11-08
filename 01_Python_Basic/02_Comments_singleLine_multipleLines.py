f4abbdd121fd1d60ec702fe4e31f6f54041a6613
Longistamina <dophamthelong03@gmail.com>
2025-11-08 15:40:41 +0900

    minor changes

--- 01_Python_Basic/02_Comments_singleLine_multipleLines.py
+++ 01_Python_Basic/02_Comments_singleLine_multipleLines.py
@@ -12,9 +12,9 @@ The planets were positioned accurately and moved in perfect harmony.
 
 Amazed, the friend asked, “This is incredible! Who made it?"
 Newton calmly replied, “No one. It just formed itself—particles randomly came together to create it.”
-The friend laughed and said, “Come on, that’s impossible. Someone clearly designed and built this!”
+The friend laughed and said, “Come on, that's impossible. Someone clearly designed and built this!”
 
-Newton looked at him and said, “If you can’t believe that this small model came about by chance, 
+Newton looked at him and said, “If you can't believe that this small model came about by chance, 
 then how can you believe that the real solar system, infinitely more complex and vast, 
 came into existence without a Creator?"
 '''
