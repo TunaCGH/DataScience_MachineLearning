f74a9b2143b2a816df8adc7e01fb47d8d3d97d3b
Longistamina <dophamthelong03@gmail.com>
2025-11-13 10:42:38 +0900

    reformatted space characters

--- 01_Python_Basic/07_input_eval.py
+++ 01_Python_Basic/07_input_eval.py
@@ -7,17 +7,17 @@
 x = int(input("Input variable x: "))
 y = int(input("Input variable y: "))
 
-print("Sum x + y =", x+y)
-print("Subtract x - y =", x-y)
-print("Multiplication x*y = ", x*y)
-print("Division x/y =", x/y)
+print("Sum x + y =", x + y)
+print("Subtract x - y =", x - y)
+print("Multiplication x*y = ", x * y)
+print("Division x/y =", x / y)
 
 
 #--------------------------------------------------------------------------------------#
 #---------------------------------------- eval() --------------------------------------#
 #--------------------------------------------------------------------------------------#
 
-k1 = eval('2+3+5') # eval considers this as a mathematic expression, hence returns 10 as the result of 2+3+5
+k1 = eval('2 + 3 + 5') # eval considers this as a mathematic expression, hence returns 10 as the result of 2+3+5
 print(f'''
 k1 = {k1}
 type_k1 = {type(k1)}
@@ -35,13 +35,13 @@ k3 = {k3}
 type_k3 = {type(k3)}
 \n''')
 
-k4 = eval('20,23,45') # eval considers this as a tuple, hence returns (20, 23, 45) as a tuple
+k4 = eval('20, 23, 45') # eval considers this as a tuple, hence returns (20, 23, 45) as a tuple
 print(f'''
 k4 = {k4}
 type_k4 = {type(k4)}
 \n''')
 
-k5 = eval('[12,13,14,15]') # eval considers this as a list, hence returns [12, 13, 14, 15] as list
+k5 = eval('[12, 13, 14, 15]') # eval considers this as a list, hence returns [12, 13, 14, 15] as list
 print(f'''
 k5 = {k5}
 type_k5 = {type(k5)}
